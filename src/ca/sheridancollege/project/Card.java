7178a469ddfddcb775756df5f67b41b3b5427e41
Armaan7637 <122410097+Armaan7637@users.noreply.github.com>
2023-03-12 16:37:06 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -9,7 +9,7 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  *
- * @author Armaandeep Singh
+ * @author Armaandeep Singh, Amanpreet Kaur, Datardeep Singh, Syed Abidi
  * 10 March, 2023
  */
 public abstract class Card {
